295889c21166adead69ffd9beb5eae5b8a8456d0
Jhonatan Zubieta <yosuer15@hotmail.com>
2016-10-01 00:09:08 -0300

    Init

---
+++ src/main/java/com/yosuer/DockerBootApplication.java
@@ -0,0 +1,12 @@
+package com.yosuer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerBootApplication.class, args);
+	}
+}
